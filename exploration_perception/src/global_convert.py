2e88c16aadb4ea72d297877e6372a85781335c21
Alex <47084301+aswift8@users.noreply.github.com>
2019-12-20 15:47:56 +0000

    Cleaned global_convert.py
    Cleaned comments and code.

--- exploration_perception/src/global_convert.py
+++ exploration_perception/src/global_convert.py
@@ -14,7 +14,7 @@ from exploration_perception.msg import DangerSign
 from std_msgs.msg import Float32MultiArray
 from sensor_msgs.msg import Image
 
-# Change from sign int to name
+# Change from sign number to name
 sign_to_name = {
 	0: "biohazard",
 	1: "danger",
@@ -26,7 +26,7 @@ sign_to_name = {
 	7: "alive worker",
 	8: "dead worker"
 }
-# Change from object int to sign int
+# Change from object number to sign number
 object_to_sign = {
 	1: 3,
 	2: 3,
@@ -87,165 +87,135 @@ object_to_sign = {
 	57: 6
 }
 
-redFound = False
-greenFound = False
-
-# Determine whether a worker is dead or alive
+"""
+Determines whether a worker is dead or alive.
+Return: "dead" if dead, "alive" if alive.
+"""
 def determine_worker(image_message, width, height, homography_matrix):
-	global redFound
-	global greenFound
-	# Get cropped cv image
-	print("Checking worker! {},{}, len(mat):{}".format(width, height, len(homography_matrix)))
+	# Convert image message to OpenCV image
 	bridge = CvBridge()
 	cv_img = bridge.imgmsg_to_cv2(image_message, "bgr8")
+	# Crop image to the relevant sign section
+	# Build homography matrix
 	h = np.matrix([[homography_matrix[0], homography_matrix[1], homography_matrix[2]],
 				   [homography_matrix[3], homography_matrix[4], homography_matrix[5]],
 				   [homography_matrix[6], homography_matrix[7], homography_matrix[8]]]).T
+	# Get bounding box corners in image frame
 	in_points = np.array([[[0, 0], [width, 0], [0, height], [width, height]]])
-	out_points = cv2.perspectiveTransform(in_points, h)[0]
-	rospy.logwarn("POINTS\n"+str(out_points))
-	rospy.logwarn("MATRIX\n"+str(h))
-	rospy.logwarn("OUT SHAPE: "+str(out_points.shape))
+	out_points = cv2.perspectiveTransform(in_points, h)[0]	# Only top-left corner is used, approximations used for width and height
+	# Unpack x, y for top-left corner and convert to integer
 	x, y = list(out_points[0])
 	x, y = int(x), int(y)
+	# Crop the image
 	cv_sign_img = cv_img[y:y+int(height), x:x+int(width)]
-	# CLAHE
-	cv_sign_hsv = cv2.cvtColor(cv_sign_img, cv2.COLOR_BGR2HSV)
-	h, s, v = cv2.split(cv_sign_hsv)
-	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-	v = clahe.apply(v)
-	cv_sign_hsv = cv2.merge([h, s, v])
-	cv_sign_img = cv_sign_hsv#cv2.cvtColor(cv_sign_hsv, cv2.COLOR_HSV2BGR)
-	# Create histograms
+	# Convert image to HSV - histogram to be made from Hue channel
+	cv_sign_img = cv2.cvtColor(cv_sign_img, cv2.COLOR_BGR2HSV)
+	# Create histogram - initially all channels were used for histograms, but hue was found to be most useful
 	# Colour space ranges
-	colour_space_ranges = [180, 256, 256]# [180, 256, 256]   # HSV
+	colour_space_ranges = [180, 256, 256]	# H, S, V
 	bin_divider = 10
+	# (range)//(bin_divider) bins for each channel
 	bin_counts = [num // bin_divider for num in colour_space_ranges]
-	ch1, ch2, ch3 = cv2.split(cv_sign_img)  # Separate channels
+	ch1, ch2, ch3 = cv2.split(cv_sign_img)  # Separate channels from image
+	# Histogram of "hue" channel
 	hist1 = cv2.calcHist([ch1], [0], None, [bin_counts[0]], [0, colour_space_ranges[0]])
-	hist2 = cv2.calcHist([ch2], [0], None, [bin_counts[1]], [0, colour_space_ranges[1]])
-	hist3 = cv2.calcHist([ch3], [0], None, [bin_counts[2]], [0, colour_space_ranges[2]])
+	# Normalise the histogram so a more meaningful comparison to threshold can be made
 	cv2.normalize(hist1, hist1)
-	cv2.normalize(hist2, hist2)
-	cv2.normalize(hist3, hist3)
-	#cv2.imshow("sign", cv_sign_img)
-	#cv2.waitKey(0)
-	#g_sum = sum([i * hist2[i] for i in range(len(hist2))])	# how green it is
-	#r_sum = sum([i * hist3[i] for i in range(len(hist3))])	# how red it is
-	#result = "green" if g_sum > r_sum else "red"
+	# Bins 5, 6, 7, 8 were found to have the greatest difference between "alive" and "dead" worker
 	green_sum = sum(hist1[5:9])
+	# Compare the sum to the threshold value. If there's more "green" than threshold then the worker is alive, otherwise it's dead
 	result = "alive" if green_sum > 0.05 else "dead"
-	result += str(green_sum)
-	display_hists([hist1, hist2, hist3], result)
-	if result == "alive":
-		greenFound = True
-	elif result == "dead":
-		redFound = True
-	#cv2.destroyAllWindows()
-	return result		# is it more green than red?
+	return result
 
-def display_hists(histograms, title):
-	global redFound
-	global greenFound
-	fig, ax = plt.subplots(len(histograms))
-	fig.canvas.set_window_title(title)
-	for i, hist in enumerate(histograms):
-		ax[i].plot(np.arange(len(hist)), hist.ravel())
-	if redFound == False:
-		plt.savefig("../catkin_ws/src/exploration_main/result/histred.png")
-	if greenFound == False:
-		plt.savefig("../catkin_ws/src/exploration_main/result/histgreen.png")
-
-#Define Interrupt method for when data is recieved from topic
+"""
+Listener to the "/object" topic where find_object_2d publishes information on detected objects.
+Determine what sign corresponds to the detected object, publish the sign name and map pose to "/danger_signs".
+"""
 def object_listener(msg):
-	global seq
-	global pub
-	rospy.logdebug("Entered callback")
+	global seq		# Used when creating the message header
+	global pub		# "/danger_signs" publisher
 	if len(msg.data) == 0:
 		return
 
-	#Data array element zero and generate object name
+	# Message index 0 is the object number
 	object_index = int(msg.data[0])
-	object_name = "object_"
-	object_frame = object_name + str(object_index)
-	rospy.logdebug("About to enter try/catch")
+	object_frame = "object_" + str(object_index)
 	
-	#Attempt to lookup tranform 
+	# Try/catch required for transform_listener.lookupTransform
 	try:
-		rospy.logdebug("Entered try")
+		# Lookup transform of object to map (global cooridnates)
+		(trans,rot) = transform_listener.lookupTransform('map', object_frame, rospy.Time(0))
 
-		#Lookup transform of object to map (global cooridnates)
-		(trans,rot) = listener.lookupTransform('map',object_frame, rospy.Time(0))
-		rospy.logdebug("After lookup")
-
-		#Create empty message to send data
+		# Instantiate message
 		object_Pose = DangerSign()
-		rospy.logdebug("Created Message")
 
-		#Add position info to message
+		# Add pose to message
 		object_Pose.sign_pose.pose.position.x = trans[0]
 		object_Pose.sign_pose.pose.position.y = trans[1]
 		object_Pose.sign_pose.pose.position.z = trans[2]
-		rospy.logdebug("Assigned linear positions: ")
-
-		#Add rotation info to message
 		object_Pose.sign_pose.pose.orientation.x = rot[0]
 		object_Pose.sign_pose.pose.orientation.y = rot[1]
 		object_Pose.sign_pose.pose.orientation.z = rot[2]
 		object_Pose.sign_pose.pose.orientation.w = rot[3]
-		rospy.logdebug("Assigned angular positions: ")
 
-		if object_index in object_to_sign:  #Lookup marker ID based from dictionary
-			rospy.logdebug("Found Object in dictionary")
-			# Convert object num to sign ID
+		# Ensure object number is in object-sign dictionary
+		if object_index in object_to_sign:
+			# Convert object number to sign ID
 			sign_id = object_to_sign[object_index]
-			# Check for worker
+			# If a worker was detected more checks need to be made
 			if sign_id == 6:
+				# determine_worker requires the decompressed Image message as well as the Float32MultiArray message
 				global image_msg
 				if image_msg is None:
+					# Will fire if this block is entered before the Image subscriber receives the first image
+					# Shouldn't happen, but also shouldn't break anything
 					rospy.logerr("Worker detected but image not stored yet")
 				else:
+					# Width and Height of detected object are in index 1 and 2 of the Float32MultiArray
 					width = msg.data[1]
 					height = msg.data[2]
+					# Indices 3 through 11 make up the homography matrix of the detected object
 					homography_matrix = msg.data[3:12]
-					# "alive" or "dead"
+					# determine_worker returns either "alive" or "dead"
 					worker_type = determine_worker(image_msg, width, height, homography_matrix)
+					# Set the sign_id based on return string
 					# Alive worker - 7, dead worker - 8
 					sign_id = 7 if worker_type=="alive" else 8
-			# Set pose ID to determined sign ID
+			# Look up the sign name from the sign_to_name dictionary
 			object_Pose.sign_name.data = sign_to_name[sign_id]
-			rospy.logwarn("The detected sign is {}".format(object_Pose.sign_name.data))
+		else:
+			# Object number wasn't in object-sign dictionary, use a default name
+			object_Pose.sign_name.data = "Unknown sign found"
 
-
-		else:							   #If not in dictionary set it to default value
-			rospy.logdebug("Object not in dictionary") 
-			object_Pose.sign_name.data = "No object found"
-
+		# Use sequence in header, increment sequence
 		object_Pose.sign_pose.header.seq = seq
 		seq += 1
-
 		#Publish message
-		rospy.logdebug("Publishing Message")
 		pub.publish(object_Pose)
 
 	except(tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+		# Exception occurred when trying to lookup object transform. Provide warning and log error.
 		rospy.logwarn("Object not in frame" + object_frame)
 		rospy.logerr(e.message)
-
-		if object_index in object_to_sign:  #Lookup marker ID based from dictionary
+		# If sign type determined say which it is
+		if object_index in object_to_sign:
 			rospy.logwarn("Sign: " + sign_to_name[object_to_sign[object_index]])
 
+"""
+Listener to the "/simplified_image/decompressed" topic, used to store the most recent decompressed image
+Required for the determine_worker method
+"""
 def image_storage_listener(msg):
 	global image_msg
 	image_msg = msg
 
 
-rospy.init_node("dangerSignsNode")
-listener = tf.TransformListener()	# Create listener
-seq = 0					# Used in creating object_Pose.sign_pose.header
-image_msg = None			# Need to keep image message for worker determination
-sub = rospy.Subscriber("/objects", Float32MultiArray, object_listener)	# Object subscriber
+rospy.init_node("dangerSignsNode")			# Initialise node
+transform_listener = tf.TransformListener()	# Create transform listener
+seq = 0										# Used in creating object_Pose.sign_pose.header
+image_msg = None							# Need to keep image message for worker determination
+sub = rospy.Subscriber("/objects", Float32MultiArray, object_listener)							# Object subscriber
 image_sub = rospy.Subscriber("/simplified_image/decompressed", Image, image_storage_listener)	# Image subscriber
-pub = rospy.Publisher("/danger_signs", DangerSign, queue_size=1)	# Sign publisher
+pub = rospy.Publisher("/danger_signs", DangerSign, queue_size=1)								# Sign publisher
 rospy.spin()
 
