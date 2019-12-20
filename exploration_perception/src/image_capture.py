55a430bcb6f5d65f650546fbf3397dd4bfb4b8c2
Alex <47084301+aswift8@users.noreply.github.com>
2019-12-20 16:23:20 +0000

    Perception cleanup
    Removed unused files, renamed some files, updated some comments.

--- exploration_perception/src/image_capture.py
+++ exploration_perception/src/image_capture.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python 
+# Subscribes to raw image topic, displays image using OpenCV, 'Q' used to save to file
+# Note: uses a hard-coded directory for the image path
 
 import rospy
 import cv2
@@ -8,27 +10,34 @@ from cv_bridge import CvBridge, CvBridgeError
 i = 0
 middle = 0.0
 
+"""
+Obtains middle laser value in scan, used during testing
+"""
 def laser_cb(msg):
 	global middle
 	middle = msg.ranges[len(msg.ranges) // 2]
 
+"""
+Image subscriber. Displays image using OpenCV, image can then be saved to file.
+"""
 def image_sub(data):
 	global i 		
 	try:
 		cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
-		#returns and stores last key pressed on keyboard by user
+		# Returns and stores last key pressed on keyboard by user
 		cv2.imshow("image", cv_image)
 		key = cv2.waitKey(2000) & 0xFF
-		#check if key corresponds to esc (quit), if yes quit the video capture
+		# Check if key corresponds to q (take picture), if yes save image to file
 		if key == ord('q'):
 			cv2.putText(cv_image, "laser value: %1.2f" %middle, (50, 50), 
 				cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2, 0)
+			# Hard-coded directory, would need to be changed on a different machine
 			cv2.imwrite("/home/craig_ros/Pictures/test/image{}.png".format(i), cv_image)
 			i += 1			
 	except CvBridgeError as e:
 		print(e)
 	
-rospy.init_node("a_node")
+rospy.init_node("image_capture")
 bridge = CvBridge()
 subscriber = rospy.Subscriber("/camera/rgb/image_rect_color", Image, image_sub) 
 laser_subscriber = rospy.Subscriber("/scan", LaserScan, laser_cb)
